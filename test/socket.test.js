2c104974370abf7a3550afb42ba149a3ed6b1151
jorge <jbousing@gmail.com>
2024-03-30 11:24:02 -0400

    test example with jest and socket.io

--- test/socket.test.js
+++ test/socket.test.js
@@ -1,4 +1,4 @@
-const createServer = require("http");
+const { createServer } = require("http");
 const { Server } = require("socket.io");
 const Client = require("socket.io-client");
 
@@ -10,6 +10,7 @@ describe("Testing Socket.io", () => {
     beforeAll((done) => {
         const httpServer = createServer();
         io = new Server(httpServer);
+        //console.log(io);
 
         httpServer.listen(() => {
             const port = httpServer.address().port;
@@ -19,12 +20,43 @@ describe("Testing Socket.io", () => {
                 serverSocket = socket;
             });
 
+            //console.log(io);
             clientSocket.on("connect",done);
         });
     });
 
     afterAll(() => {
+        
         io.close();
         clientSocket.close();
+    });
+
+    test("test event", (done) => {
+        clientSocket.on("greeting", (greet) => {
+            try {
+                expect(greet).toBe("hola mundo");
+                done();
+            } catch (error) {
+                done(error);
+            }
+        });
+
+        serverSocket.emit("greeting","hola mundo");
+    });
+
+    test("Testing callbacks (acknoledgements)", (done) => {
+        
+        serverSocket.on("bark", (callback) => {
+            callback("uya");
+        });
+
+        clientSocket.emit("bark",(arg) => {
+            try {
+                expect(arg).toBe("uya");
+                done();
+            }catch (error) {
+                done(error);
+            }   
+        })
     });
 });
